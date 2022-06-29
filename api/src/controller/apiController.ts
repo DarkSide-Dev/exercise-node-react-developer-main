e9ffd89985500d6224cab6d91daa347b34ee7958
Joao Oliveira <vitinho.oliv@gmail.com>
2022-06-28 21:04:18 -0300

    feat: installing axios and creating requisition from the source

--- api/src/controller/apiController.ts
+++ api/src/controller/apiController.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
+
+import { Repo } from '../models/Repo';
 
 export const repo = async (_: Request, res: Response) => {
-    res.header('Cache-Control', 'no-store');
-  
-    res.status(200);
-  
-    // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-    res.json([]);
+
+    try{
+
+        res.header('Cache-Control', 'no-store');
+        res.header('Content-Type', 'application/json')
+    
+        res.status(200);
+
+        let url: string = process.env.API_BASE as string;
+
+        const data = await axios.get<Repo[]>(url).then((repositories) => {
+        
+            let filteredRepo: Repo[] = repositories.data.filter(item => {
+                return !item.fork;
+            });
+    
+            return filteredRepo;
+        }).catch(() => {throw 'Requisiton failed'});
+
+        res.send(JSON.stringify(data, null, 2));
+
+    } catch(error){
+        res.status(503);
+        res.json({msg: error});
+    }
 }
